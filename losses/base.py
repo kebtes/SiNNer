66149d31bb1e0a5aceb348d209e168239a5f17de
Kibrewossen <47031138+kebtes@users.noreply.github.com>
2025-04-12 18:49:52 +0300

    Loss calculators

---
+++ losses/base.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+class Loss:
+    """
+    --------------------------------
+    BASE CLASS FOR THE LOSS FUNCTION
+    --------------------------------
+    """
+
+    def calculate(self, output, y):
+        """
+        The function calculates the mean loss across all samples in a batch based on the output and
+        target values.
+        """
+
+        # loss calculated for each individual sample in the batch
+        sample_losses = self.forward(output, y)
+
+        # mean loss accross all the samples in the batch
+        data_loss = np.mean(sample_losses)
+        return data_loss
+    
+    def forward(self, y_pred, y_true):
+        """
+        Forward pass to calculate the loss
+
+        """
+
+        raise NotImplementedError
+    
+    def backward(self, dvalues, y_true):
+        """
+        Backward pass to calculate gradients
+        
+        """
+
+        raise NotImplementedError
