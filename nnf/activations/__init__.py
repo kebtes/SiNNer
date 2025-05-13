633358d0008086b98effac72cf59bf3005d3552b
Kibrewossen <47031138+kebtes@users.noreply.github.com>
2025-05-14 00:05:27 +0300

    Add Tanh activation function to the module exports

--- nnf/activations/__init__.py
+++ nnf/activations/__init__.py
@@ -3,5 +3,7 @@ from nnf.activations.relu import ReLU
 from nnf.activations.softmax import Softmax
 from nnf.activations.sigmoid import Sigmoid
 from nnf.activations.leaky_relu import LeakyReLU
+from nnf.activations.tanh import Tanh
 
-__all__ = ['Activation', 'ReLU', 'Softmax', 'Sigmoid', 'LeakyReLU']
+
+__all__ = ['Activation', 'ReLU', 'Softmax', 'Sigmoid', 'LeakyReLU', 'Tanh']
