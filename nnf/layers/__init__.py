9833d5526a7f88646b12c1b425296f65d9c0a719
Kibrewossen <47031138+kebtes@users.noreply.github.com>
2025-05-17 14:11:45 +0300

    Add Dropout layer implementation

--- nnf/layers/__init__.py
+++ nnf/layers/__init__.py
@@ -1,4 +1,5 @@
 from nnf.layers.dense import Dense
-from nnf.layers.dense import Layer
+from nnf.layers.base import Layer
+from nnf.layers.dropout import Dropout
 
-__all__ = ['Dense', 'Layer']
+__all__ = ['Dense', 'Layer', 'Dropout']
